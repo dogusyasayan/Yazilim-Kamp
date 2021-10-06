da0f5b42c121d61d2bb84e34133627227cf5194d
Dogus Yasayan <dogusyasayans@gmail.com>
2021-10-06 18:00:52 +0300

    06.10.2021

---
+++ Example/MyList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Example
+{
+    class MyList<T>
+    {
+
+        //Bir dizi oluşturduk.
+        T[] items;
+
+        //Constractor ile dizi sayısının ilk halini verdik.
+        public MyList()
+        {
+            items = new T[0];
+        }
+
+        public void Add(T item)
+        {
+            T[] tempArray = items;
+            items = new T[items.Length + 1]; // new lenince yeni bir referans değeri aldı
+
+            for (int i = 0; i <tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+            items[items.Length - 1] = item;
+        } 
+
+        public int Leghth
+        {
+            get { return items.Length; }
+        }
+
+        public T[] Items
+        {
+            get { return Items}
+        }
+
+
+
+
+    }
+}
---
+++ Example/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1-) List metodunu kullan,ekrana listeyi yaz.
+
+            List<string> TeamList = new List<string>();
+
+            TeamList.Add("Fenerbahce");
+            TeamList.Add("Galatasaray");
+            TeamList.Add("Beşiktaş");
+            TeamList.Add("Trabzonspor");
+
+            foreach (var takim in TeamList)
+            {
+                Console.WriteLine(takim);
+            }
+
+            // 2-) Kendi Listeni olustur, (MyList) bu liste için ekleme ve silme metodu yaz
+            MyList<string> teams = new MyList<string>();
+            teams.Add("Dogus");
+            Console.WriteLine(teams.Leghth);
+
+
+        }
+    }
+}
